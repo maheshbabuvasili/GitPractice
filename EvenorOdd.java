c0de74fd33cd71aa008c19385be88ee19957bb56
maheshbabuvasili <maheshbabuc032@gmail.com>
2023-03-02 20:01:35 +0530

    completed Even or Odd program

--- EvenorOdd.java
+++ EvenorOdd.java
@@ -4,3 +4,12 @@ public static void main(String[] args){
 Test.checkEvenorOdd(12);
 }
 }
+class Test
+{
+public static void checkEvenorOdd(int a){
+if(a%2==0)
+System.out.println(a+ " is a Even number);
+else
+System.out.println(a+ " is a Odd number);
+}
+}
